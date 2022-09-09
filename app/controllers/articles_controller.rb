b3ed997a039988573b9b883339475cd95865f4ac
Zion Miller <djzionmiller@gmail.com>
2022-09-09 10:50:00 -0700

    commit tree 1e722002c82e26924df94ea5b279f7e64734c363

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,25 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # If this is the first request this user has made, 
+    # set session[:page_views] to an initial value of 0.
+    session[:page_views] ||= 0
+    # For every request to /articles/:id, increment 
+    # the value of session[:page_views] by 1.
+    session[:page_views] += 1
+
+    # If the user has viewed 3 or fewer pages, render a 
+    # JSON response with the article data
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    # If the user has viewed more than 3 pages, render a 
+    # JSON response including an error message, and a 
+    # status code of 401 unauthorized.
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
+
   end
 
   private
